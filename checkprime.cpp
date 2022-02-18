6a6512099b78f858ba4c53f745831ce0755bc8de
S2613 <100001172+S2613@users.noreply.github.com>
2022-02-19 05:18:24 +0530

    Add files via upload

---
+++ checkprime.cpp
@@ -0,0 +1,41 @@
+#include<stdio.h>  //whether given number prime or not
+#include<math.h>
+main()
+{
+	int n,i,status=1;
+	printf("enter number\n");
+	scanf("%d",&n);
+	if(n==1)
+	{
+		printf("not a prime");
+		status=0;
+	}
+	else if(n==2)
+	{
+		printf("a prime");
+		status=0;
+	}
+	else if(n%2==0)
+	{
+		printf("not a prime");
+		status=0;
+	}
+	else if(n>=3)
+	{
+		i=3;
+		while(i<=sqrt(n) &&status==1)
+		{
+		
+		    if(n%i==0)
+		   {
+		     printf("not a prime\n");
+			 status=0;	
+		   }
+		   i=i+2;
+		}
+	}
+	if(status==1)
+	{
+		printf("prime number\n");
+	}	
+}
